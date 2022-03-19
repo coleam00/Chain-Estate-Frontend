4b42ab54bc36e618ed2096559733212a0c2f12ae
Cole Medin <medin169@umn.edu>
2022-03-19 13:08:41 -0500

    Renamed the CreateNFTs wrapper page to CreateNFTsPage

--- pages/createnfts.js
+++ pages/createnfts.js
@@ -8,7 +8,7 @@ import chainConfig from "../chain-config.json";
 
 import CreateNFTs from "../components/CreateNFTs";
 
-export default function CreateNFTs(props) {
+export default function CreateNFTsPage(props) {
     const { account, chainId } = useEthers();
     const networkName = chainId ? chainConfig["chainIds"][chainId] : "Not Connected";
     const CHESAddress = chainId ? chainConfig["CHESTokenAddresses"][networkName] : constants.AddressZero;
